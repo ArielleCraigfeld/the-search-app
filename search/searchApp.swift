50eee79335abc394ee5363c925db03b308b21ef9
Arielle S. Craigfeld <ariellecraigfeld@gmail.com>
2025-01-24 11:40:52 -0500

    Initial Commit

---
+++ search/searchApp.swift
@@ -0,0 +1,17 @@
+//
+//  searchApp.swift
+//  search
+//
+//  Created by Arielle Craigfeld on 1/24/25.
+//
+
+import SwiftUI
+
+@main
+struct searchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
